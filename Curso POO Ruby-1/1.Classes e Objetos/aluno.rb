0ddbb8aac0a1c90710e3ac54e32ef6020149d947
AngeloSouza1 <angeloafdesouza@gmail.com>
2023-09-13 16:34:49 -0300

    Curso 1: POO

---
+++ Curso POO Ruby-1/1.Classes e Objetos/aluno.rb
@@ -0,0 +1,16 @@
+class Aluno 
+  attr_reader :nome, :idade
+
+  def initialize(nome, idade)
+    @nome = nome
+    @idade= idade
+  end
+
+  def mostrar_dados
+    puts "Nome: #{@nome}, Idade: #{self.idade}"
+  end
+end
+
+a1 = Aluno.new("Angelo", 42)
+
+a1.mostrar_dados
