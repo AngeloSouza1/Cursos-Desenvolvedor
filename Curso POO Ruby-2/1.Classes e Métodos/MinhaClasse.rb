54e82547fb3905e33d05ca575a56dc2b4077b65f
AngeloSouza1 <angeloafdesouza@gmail.com>
2023-09-14 21:05:13 -0300

    Curso POO Ruby-2

---
+++ Curso POO Ruby-2/1.Classes e Métodos/MinhaClasse.rb
@@ -0,0 +1,8 @@
+class MinhaClasse
+    def imprimir_ola(nome)
+        puts "Olá #{nome}"
+    end
+end
+
+objeto = MinhaClasse.new
+objeto.imprimir_ola("Angelo")
---
+++ Curso POO Ruby-2/5.Abstração/modulo.rb
@@ -0,0 +1,11 @@
+module Comunica
+    def comunicar
+        puts "Ola, sou a classe mãe"
+    end
+end
+
+module Operacoes
+    def subt(x,y)
+        x-y
+    end
+end
