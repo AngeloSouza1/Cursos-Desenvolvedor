c8875e0693a4121a615a13e91e6149ce5b25420e
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-02-03 11:04:14 -0300

    feat : code ruby

---
+++ Curso - Onebit - Ruby/2-ArquivoGems/9-faker.rb
@@ -0,0 +1,10 @@
+require "faker"
+
+puts Faker.class
+puts Faker::Name.class
+puts Faker::Name.name
+puts Faker::Name.first_name
+puts Faker::Name.last_name
+
+puts Faker::Music.instrument
+puts Faker::Sports::Football.player
