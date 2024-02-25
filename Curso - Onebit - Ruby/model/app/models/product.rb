72a80a92dc9f544c70960df639445b1da15df7a9
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-02-25 16:30:00 -0300

    Chore: Update

--- Curso - Onebit - Ruby/model/app/models/product.rb
+++ Curso - Onebit - Ruby/model/app/models/product.rb
@@ -1,2 +1,8 @@
-class Product < ApplicationRecord
-end
+    class Product < ApplicationRecord
+        validates :name, presence: true
+        validates_uniqueness_of :name
+        validates_length_of :name, maximum: 5
+    end
+
+
+
