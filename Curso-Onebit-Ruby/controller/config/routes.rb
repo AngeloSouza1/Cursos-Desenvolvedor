b6c28ab6e4e334bae18518f1dcf1bcf93ba07954
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-03-13 09:58:06 -0300

    chore: update]

---
+++ Curso-Onebit-Ruby/controller/config/routes.rb
@@ -0,0 +1,26 @@
+Rails.application.routes.draw do
+    get "up" => "rails/health#show", as: :rails_health_check
+    get "simple", controller: "simple", action: "hello"
+    get 'simple/hello', defaults: { format: 'json' }
+    get 'simple2', to: 'simple#hello', defaults: { format: 'json' }
+ 
+    get "user/show/:id", to: "user#show", defaults: { format: 'json' }
+
+    get '/tasks', to: 'tasks#index'
+    get '/tasks/:id', to: 'tasks#show'
+
+    resources :posts do
+        member do
+          get 'comments', to: 'posts#comments'
+          get 'new_comment', to: 'posts#new_comment'
+          post 'create_comment', to: 'posts#create_comment'
+        end
+      # Defines the root path route ("/")
+      # root "posts#index"
+    end
+
+
+    get "example/time_sensitive", to: "example#time_sensitive_action"
+
+
+end
