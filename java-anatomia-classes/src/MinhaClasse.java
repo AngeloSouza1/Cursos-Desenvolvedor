e875fbff78113269258e800872437d75aef36c2a
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-06-10 13:55:14 -0300

    chore: convenção java

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,7 +1,15 @@
 public class MinhaClasse {
 
     public static void main (String [] args){
-        System.out.println("Olá, bem-vindo!");
+        // System.out.println("Olá, bem-vindo!");
+
+        String primeiroNome = "Angelo";
+        String segundoNome = "Souza";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);   
     }
-    
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+        return "Resultado do método: " + primeiroNome.concat(" ").concat(segundoNome);
+    }
 }
