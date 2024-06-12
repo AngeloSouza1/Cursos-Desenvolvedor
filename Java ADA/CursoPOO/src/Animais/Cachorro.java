cf85314d8c31e087a9e567412c8a183f5ec26e99
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-06-12 15:44:03 -0300

    feat: curso javascript - ADA - encapsulamento e construtores

--- Java ADA/CursoPOO/src/Animais/Cachorro.java
+++ Java ADA/CursoPOO/src/Animais/Cachorro.java
@@ -1,13 +1,79 @@
 package Animais;
 
 public class Cachorro {
+//atributos
+    private String nome;
+    private  String cor;
+    private  int altura;
+    private  double peso;
+    private  int tamanhoDoRabo;
+    private String estadoDeEspirito;
 
-    public String nome;
-    public String cor;
-    public int altura;
-    public double peso;
-    public int tamanhoDoRabo;
-    public String estadoDeEspirito;
+
+ //construtores
+ 
+
+ 
+ //métodos
+    public String getNome() {
+        return nome;
+    }
+
+
+  //construtores
+    public Cachorro(){}
+
+    public Cachorro(String nome, String cor, int altura, double peso, int tamanhoDoRabo, String estadoDeEspirito) {
+        this.nome = nome;
+        this.cor = cor;
+        this.altura = altura;
+        this.peso = peso;
+        this.tamanhoDoRabo = tamanhoDoRabo;
+        this.estadoDeEspirito = estadoDeEspirito;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public int getTamanhoDoRabo() {
+        return tamanhoDoRabo;
+    }
+
+    public void setTamanhoDoRabo(int tamanhoDoRabo) {
+        this.tamanhoDoRabo = tamanhoDoRabo;
+    }
+    public String getEstadoDeEspirito() {
+        return estadoDeEspirito;
+    }
+
+    public void setEstadoDeEspirito(String estadoDeEspirito) {
+        this.estadoDeEspirito = estadoDeEspirito;
+    }
 
     public void comer(){}
 
@@ -19,15 +85,25 @@ public class Cachorro {
         return "Bolinha";
     }
 
-    public String interagir(String acao){
-         if (acao.equals("carinho")){
-            this.estadoDeEspirito = "feliz";
-         }else if (acao.equals("vai dormir!")){
-            this.estadoDeEspirito = "Bravo";
-         }else{
-            this.estadoDeEspirito = "Neutro";
-         }   
-        return estadoDeEspirito;    
+    
+    public String interagir(String acao) {
+        switch (acao) {
+            case "carinho":
+                this.estadoDeEspirito = "feliz";
+                break;
+            case "vai dormir!":
+                this.estadoDeEspirito = "Bravo";
+                break;
+            default:
+                this.estadoDeEspirito = "Neutro";
+                break;
+        }
+        return estadoDeEspirito;
     }
+    
+
+
+
+
 
 }
--- Java ADA/CursoPOO/src/App.java
+++ Java ADA/CursoPOO/src/App.java
@@ -7,21 +7,21 @@ public class App {
         Cachorro cao = new Cachorro();
 
         
-        cao.nome = "MAX";
-        cao.cor = "Preto";
-        cao.altura = 40;
-        cao.peso = 30;
-        cao.tamanhoDoRabo = 15;
+        Cachorro cao2 = new Cachorro("REX", "Marron", 35,50,16,"nada");
+             
+        // cao.setNome("MAX");
+        // cao.setCor("Preto");
+        // cao.setAltura(40);
+        // cao.setPeso(30);
+        // cao.setTamanhoDoRabo(15);
 
-        
-
-        // cao.latir();
-        // System.out.println("Cão pegou a " + cao.pegar());
-
-        
         System.out.println("O cão está: " + cao.interagir("carinho"));
         System.out.println("O cão está: " + cao.interagir("vai dormir!"));        
         System.out.println("O cão está: " + cao.interagir("nada"));
+
+
+        System.out.println(cao2.getNome());    
+        System.out.println(cao2.getPeso());    
 
     }
 }
