4a59ecb76b8d25d7d2899c20ab15a02be960509e
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-08-01 16:22:51 -0300

    chore: validações de Unicidade[uniqueness]

--- ecommerce/app/models/fornecedor.rb
+++ ecommerce/app/models/fornecedor.rb
@@ -3,4 +3,10 @@ class Fornecedor < ApplicationRecord
     validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP, allow_blank: true
     validates_format_of :cep, with: /\A\d{5}-\d{3}\z/, allow_blank: true, message: "deve ter o formato XXXXX-XXX"
     validates_format_of :cnpj, with: /\A\d{2}.\d{3}\.\d{3}\/\d{4}-\d{2}\z/, allow_blank: true, message: "deve ter o formato XX.XXX.XXX/XXXX-XX"
+
+    validates_uniqueness_of :email, case_sensitive: false
+
+    #com combinacao
+    # validates_uniqueness_of :email, case_sensitive: false, scope: :cnpj
+
 end
