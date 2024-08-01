574d20426636fb61c1a70b3e0ef7b76bbad45e27
AngeloSouza1 <angeloafdesouza@gmail.com>
2024-08-01 15:41:45 -0300

    chore: validações numéricas

--- ecommerce/app/models/carro.rb
+++ ecommerce/app/models/carro.rb
@@ -1,5 +1,12 @@
 class Carro < ApplicationRecord
     validates_presence_of :nome, :modelo, :ano
     validates_length_of :nome, minimum: 10 , maximum: 30
+    # validates_numericality_of :ano, greater_than: 2000
+    # validates_numericality_of :ano, greater_than_or_equal_to: 2000
+    # validates_numericality_of :ano, greater_than_or_equal_to: Time.now.year
+    # validates_numericality_of :ano, less_than_or_equal_to: Time.now.year
+    # validates_numericality_of :ano, odd: true # impar
+    # validates_numericality_of :ano, odd: true # par
+    # validates_numericality_of :ano, only_integer: true
 
 end
